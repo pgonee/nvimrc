e4a1ddf3f8386fd4d3bb258e90916bf2fa5b5c26
pgonee <pgonee@gmail.com>
2023-10-30 18:53:26 +0900

    update

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 require("packer").startup(function(use)
     use("wbthomason/packer.nvim")
     use("neovim/nvim-lspconfig")
-    use("scrooloose/nerdtree")
     use("junegunn/goyo.vim")
     use("preservim/nerdcommenter")
     use("airblade/vim-gitgutter")
@@ -46,11 +45,17 @@ require("packer").startup(function(use)
     use("jremmen/vim-ripgrep")
     use("mhartington/formatter.nvim")
     use("ryanoasis/vim-devicons")
+    use({
+        "nvim-tree/nvim-tree.lua",
+        requires = {
+            "nvim-tree/nvim-web-devicons", -- optional
+        },
+    })
     use("nvim-tree/nvim-web-devicons")
 
     vim.keymap.set("n", "<localleader>q", ":q<cr>", { noremap = true })
-    vim.keymap.set("n", "<localleader>dd", ":NERDTreeToggle<cr>", { noremap = true })
-    vim.keymap.set("n", "<localleader>df", ":NERDTreeFind<cr>", { noremap = true })
+    vim.keymap.set("n", "<localleader>dd", ":NvimTreeToggle<cr>", { noremap = true })
+    vim.keymap.set("n", "<localleader>df", ":NvimTreeFocus<cr>", { noremap = true })
     vim.keymap.set("n", "<localleader>tt", ":tabnew<cr>", { noremap = true })
     vim.keymap.set("n", "<localleader>tw", ":tabnext<cr>", { noremap = true })
     vim.keymap.set("n", "<localleader>tp", ":tabprev<cr>", { noremap = true })
@@ -109,11 +114,11 @@ require("packer").startup(function(use)
     telescope.load_extension("fzf")
 
     local telescope_builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files({ hidden = 1})<cr>", {})
-    vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, {})
-    vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, {})
-    vim.keymap.set("n", "<leader>fr", telescope_builtin.resume, {})
+    vim.keymap.set("n", "<localleader>ff", "<cmd>lua require('telescope.builtin').find_files({ hidden = 1})<cr>", {})
+    vim.keymap.set("n", "<localleader>fg", telescope_builtin.live_grep, {})
+    vim.keymap.set("n", "<localleader>fb", telescope_builtin.buffers, {})
+    vim.keymap.set("n", "<localleader>fh", telescope_builtin.help_tags, {})
+    vim.keymap.set("n", "<localleader>fr", telescope_builtin.resume, {})
 
     vim.keymap.set("n", "<esc>", "<c-c>", {})
 
@@ -152,19 +157,12 @@ require("packer").startup(function(use)
     vim.api.nvim_command("colorscheme solarized8")
     vim.o.colorcolumn = "120"
 
-    vim.cmd([[
-    let NERDTreeIgnore = [
-    \ '\.pyc$',
-    \ '\.class$',
-    \ '\.so$',
-    \ '\.swp$',
-    \ '__pycache__$',
-    \ '\.beam$',
-    \ 'node_modules$',
-    \ 'bower_components$'
-    \]
-    let NERDTreeShowHidden = 1
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    vim.opt.termguicolors = true
+    require("nvim-tree").setup()
 
+    vim.cmd([[
     let g:goyo_width = 120
     let g:goyo_height = '94%'
 
@@ -195,7 +193,7 @@ require("packer").startup(function(use)
             ["<c-space>"] = cmp.mapping.complete(),
             ["<c-e>"] = cmp.mapping.abort(),
             ["<cr>"] = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Replace,
+                --behavior = cmp.ConfirmBehavior.Replace,
                 select = true,
             }),
         }),
@@ -204,15 +202,18 @@ require("packer").startup(function(use)
         }, {
             { name = "buffer", keyword_length = 3 },
         }, {
-            --{ name = 'path' },
+            { name = "path" },
         }, {
-            --{ name = 'cmdline' },
+            --{ name = "cmdline" },
         }, {
             { name = "vsnip" },
         }, {
             { name = "nvim_lsp_signature_help" },
         }),
     })
+
+    require("mason").setup()
+    require("mason-lspconfig").setup()
 
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
@@ -221,18 +222,18 @@ require("packer").startup(function(use)
         capabilities = capabilities,
     })
     lspconfig.tsserver.setup({
-        filetypes = {
-            "typescript",
-            "typescriptreact",
-            "typescript.tsx",
-        },
+        --filetypes = {
+        --"typescript",
+        --"typescriptreact",
+        --"typescript.tsx",
+        --},
         capabilities = capabilities,
-        init_options = {
-            hostInfo = "neovim",
-            preferences = {
-                importModuleSpecifierPreference = "non-relative",
-            },
-        },
+        --init_options = {
+        --hostInfo = "neovim",
+        --preferences = {
+        --importModuleSpecifierPreference = "non-relative",
+        --},
+        --},
     })
     lspconfig.lua_ls.setup({
         settings = {
@@ -249,8 +250,6 @@ require("packer").startup(function(use)
         capabilities = capabilities,
     })
 
-    require("mason").setup()
-
     vim.g.ale_fix_on_save = 0
     vim.g.ale_linters = {
         javascript = { "eslint" },
@@ -262,8 +261,8 @@ require("packer").startup(function(use)
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(ev)
             local opts = { buffer = ev.buf }
-            vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+            vim.keymap.set("n", "<localleader>gD", vim.lsp.buf.declaration, opts)
+            vim.keymap.set("n", "<localleader>gd", vim.lsp.buf.definition, opts)
         end,
     })
 
