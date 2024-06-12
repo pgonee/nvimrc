695209784c503b99e24bdb270916a1deab981ef7
pgonee <pgonee@gmail.com>
2024-06-12 14:17:59 +0900

    update

--- init.lua
+++ init.lua
@@ -352,11 +352,24 @@ require("packer").startup(function(use)
         indent = {
             enable = true,
         },
-        autotag = {
-            enable = true,
-        },
         matchup = {
             enable = true,
+        },
+    })
+    require("nvim-ts-autotag").setup({
+        opts = {
+            -- Defaults
+            enable_close = true, -- Auto close tags
+            enable_rename = true, -- Auto rename pairs of tags
+            enable_close_on_slash = false, -- Auto close on trailing </
+        },
+        -- Also override individual filetype configs, these take priority.
+        -- Empty by default, useful if one of the "opts" global settings
+        -- doesn't work well in a specific filetype
+        per_filetype = {
+            ["html"] = {
+                enable_close = false,
+            },
         },
     })
 
