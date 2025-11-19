cea6d44172f9c468f4e818ec743876e771a11a3f
pgonee <pgonee@gmail.com>
2025-11-19 16:48:30 +0900

    update

--- init.lua
+++ init.lua
@@ -737,3 +737,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
         end, {})
     end,
 })
+
+vim.api.nvim_create_augroup("__formatter__", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = "__formatter__",
+    command = ":FormatWrite",
+})
