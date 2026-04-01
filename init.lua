a1da08f0db4d1fcdb1c68ef44670a5a72f3e9059
pgonee <pgonee@gmail.com>
2026-04-01 19:30:04 +0900

    update

--- init.lua
+++ init.lua
@@ -33,7 +33,6 @@ require("lazy").setup({
             "numToStr/Comment.nvim",
             opts = {},
         },
-        "airblade/vim-gitgutter",
         "tpope/vim-surround",
         {
             "folke/tokyonight.nvim",
@@ -174,13 +173,6 @@ require("lazy").setup({
         },
         "JoosepAlviste/nvim-ts-context-commentstring",
         {
-            "f-person/git-blame.nvim",
-            event = "VeryLazy",
-            opts = {
-                enabled = true,
-            },
-        },
-        {
             "FabijanZulj/blame.nvim",
             lazy = false,
             config = function()
@@ -251,6 +243,84 @@ require("lazy").setup({
                         timeout_ms = 500,
                         lsp_format = "fallback",
                     },
+                })
+            end,
+        },
+        {
+            "NeogitOrg/neogit",
+            lazy = true,
+            dependencies = {
+                "nvim-lua/plenary.nvim", -- required
+                "sindrets/diffview.nvim", -- optional
+                "m00qek/baleia.nvim", -- optional
+                "nvim-telescope/telescope.nvim", -- optional
+            },
+            cmd = "Neogit",
+        },
+        {
+            "lewis6991/gitsigns.nvim",
+            config = function()
+                require("gitsigns").setup({
+                    signs = {
+                        add = { text = "┃" },
+                        change = { text = "┃" },
+                        delete = { text = "_" },
+                        topdelete = { text = "‾" },
+                        changedelete = { text = "~" },
+                        untracked = { text = "┆" },
+                    },
+                    signs_staged = {
+                        add = { text = "┃" },
+                        change = { text = "┃" },
+                        delete = { text = "_" },
+                        topdelete = { text = "‾" },
+                        changedelete = { text = "~" },
+                        untracked = { text = "┆" },
+                    },
+                    signs_staged_enable = true,
+                    signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+                    numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+                    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+                    word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+                    watch_gitdir = {
+                        follow_files = true,
+                    },
+                    auto_attach = true,
+                    attach_to_untracked = false,
+                    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+                    current_line_blame_opts = {
+                        virt_text = true,
+                        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+                        delay = 1000,
+                        ignore_whitespace = false,
+                        virt_text_priority = 100,
+                        use_focus = true,
+                    },
+                    current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+                    sign_priority = 6,
+                    update_debounce = 100,
+                    status_formatter = nil, -- Use default
+                    max_file_length = 40000, -- Disable if file is longer than this (in lines)
+                    preview_config = {
+                        -- Options passed to nvim_open_win
+                        style = "minimal",
+                        relative = "cursor",
+                        row = 0,
+                        col = 1,
+                    },
+                    on_attach = function(bufnr)
+                        print("eunjo")
+                        local gitsigns = require("gitsigns")
+                        local function map(mode, l, r, opts)
+                            opts = opts or {}
+                            opts.buffer = bufnr
+                            vim.keymap.set(mode, l, r, opts)
+                        end
+                        map("n", "<localleader>crc", function()
+                            gitsigns.toggle_linehl()
+                            gitsigns.toggle_word_diff()
+                        end)
+                    end,
                 })
             end,
         },
